f99bef5b82eb0217b5ee4f02ed2c97520a9c0617
emna <emna.gaidii@gmail.com>
2023-11-10 23:11:56 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'miniWord';
+  selectedNumber: any;
+  selectedColor: any;
+  selectedStyle: any;
+  onColorChange(input: any) {
+    this.selectedColor = input.value;
+    console.log(this.selectedColor);
+  }
+  onNumberChange(_t6: HTMLInputElement) {
+    this.selectedNumber = _t6.value + 'px';
+    console.log(this.selectedNumber);
+  }
+  onSelectedStyle(style: any) {
+    this.selectedStyle = style.value;
+    console.log(this.selectedStyle);
+  }
 }
